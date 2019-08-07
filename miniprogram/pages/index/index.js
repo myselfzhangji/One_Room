84f7bfa1821e333560c958ff834eb1fb3ee2a3f0
zhangji <myselfzhangji@163.com>
2019-08-07 22:47:43 +0800

    fix the bug: forget to store openid
    Signed-off-by: zhangji <myselfzhangji@163.com>

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -80,6 +80,21 @@ Page({
         }
       }
     })
+
+    // 调用云函数
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => {
+        //console.log('[云函数] [login] user openid: ', res.result.openid)
+        app.globalData.openid = res.result.openid
+        wx.setStorageSync('openId', res.result.openid)
+      },
+      fail: err => {
+        console.error('[云函数] [login] 调用失败', err)
+      }
+    })
+
   },
 
   onGetUserInfo: function(e) {
